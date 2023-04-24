b8b082ba7c9066239fda3558ed310ef2c7cd712e
adrianeBoucinha <126793258+adrianeBoucinha@users.noreply.github.com>
2023-04-24 13:43:32 -0300

    Update API.js

--- js/API.js
+++ js/API.js
@@ -1,6 +1,6 @@
 //PARA QUANDO FOR CONSUMIR A API
 async function filmesAPI() {
-    const url = "https://api.themoviedb.org/3/movie/popular?api_key=a61ce0f53e410fdbdfd6803a12325c74&language=pt-BR&page=1";
+    const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apikey}&language=pt-BR&page=1`;
     const urlConvertida = await fetch (url);
     const {results} = await urlConvertida.json();
     //RESULTS é como aparece no json o OBJETO que será usado
@@ -9,7 +9,7 @@ async function filmesAPI() {
 
 //BUSCAR FILME POR NOME COMO PARAMETRO TITULO
 async function buscarFilmePorNome(title) {
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=a61ce0f53e410fdbdfd6803a12325c74&query=${title}&language=pt-BR&page=1`;
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${apikey}&query=${title}&language=pt-BR&page=1`;
     const urlConvertida = await fetch (url);
     //OBJETO RESULTS É O QUE QUERO USAR
     const { results } = await urlConvertida.json();
